78fdf0cbd73679ff7b39000b7a5d01d11b7bc349
lakdivian <noorul.km@people10.com>
2019-07-13 20:00:46 +0530

    Updates

---
+++ precache-manifest.42d33d50dcf394b306667890c062909a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "d04965fba78f3eed614c",
+    "url": "/react-todo/static/js/main.3bfa4fb7.chunk.js"
+  },
+  {
+    "revision": "556164cb2f27f52dbec6",
+    "url": "/react-todo/static/js/2.bc13e44b.chunk.js"
+  },
+  {
+    "revision": "d04965fba78f3eed614c",
+    "url": "/react-todo/static/css/main.75679953.chunk.css"
+  },
+  {
+    "revision": "61fb2fa9d183b5390f2173e79f925d26",
+    "url": "/react-todo/index.html"
+  }
+];
